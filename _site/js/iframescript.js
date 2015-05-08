f21d6b198285bd5a9c34482c1e0452ee88d56ba0
prabhasp <prabhas.pokharel@gmail.com>
2015-05-08 13:13:37 +0545

    removing _site

--- _site/js/iframescript.js
+++
@@ -1,7 +0,0 @@
-var width = 650,
-    height = 320;
-
-var scale = 3750;
-var yaw = -87;
-var pitch = 12.1 ;
-var roll = 0;
--- _site/js/indexscript.js
+++
@@ -1,7 +0,0 @@
-var width = 1000,
-    height = 400;
-
-var scale = 6000;
-var yaw = -84.5;
-var pitch = 11.1 ;
-var roll = 0;
