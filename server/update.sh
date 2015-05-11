7127fe6ec3ac161961593a78a35d38800de7d6c4
jediKnight <gautamxpratik@gmail.com>
2015-05-11 23:48:07 +0545

    added a reminder..these are not just numbers.

--- server/update.sh
+++ server/update.sh
@@ -4,5 +4,5 @@ cd ../data
 node ../server/fetcher-archiver.js
 topojson -o districts_topo.json --id-property 'distID, distID' --external-properties=data.csv --properties='district=district' --properties='dead=dead' --properties='injured=injured' --properties='GovComplete=GovComplete' --properties='GovPartial=GovPartial' --properties='OthComplete=OthComplete' --properties='OthPartial=OthPartial' --properties='population=population' --properties='deadpercent=deadpercent' --properties='injuredpercent=injuredpercent' -- districts_id.json;
 git add -A
-git commit -a -m "data updated;"
+git commit -a -m "there was an update..hope there won't ever be another one."
 git push origin gh-pages
