3a426a8d3dabd81333408f3bf410a9d4789b76c9
jediKnight <gautamxpratik@gmail.com>
2015-05-11 20:40:04 +0545

    account for changes in remote repo --> pull origin:gh-pages before committing local changes;

--- server/update.sh
+++ server/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull origin gh-pages
 cd ../data
 node ../server/fetcher-archiver.js
 topojson -o districts_topo.json --id-property 'distID, distID' --external-properties=data.csv --properties='district=district' --properties='dead=dead' --properties='injured=injured' --properties='GovComplete=GovComplete' --properties='GovPartial=GovPartial' --properties='OthComplete=OthComplete' --properties='OthPartial=OthPartial' --properties='population=population' --properties='deadpercent=deadpercent' --properties='injuredpercent=injuredpercent' -- districts_id.json;
