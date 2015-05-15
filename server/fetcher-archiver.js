111c220fcd59756953d30b0f3882636ea7aadd5f
Live session user <gautamxpratik@gmail.com>
2015-05-15 18:01:22 +0545

    Updating data

--- server/fetcher-archiver.js
+++ server/fetcher-archiver.js
@@ -2,24 +2,7 @@ var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
 
-var updateTimestamp;
-
-request("https://docs.google.com/spreadsheets/d/1tGQlS3lIW077sOUIJ1qO0Rtaz34V-CxKEySFQ2W2CgA/export?format=csv&gid=1901101589", function(error, response, data){
-    if (error || response.statusCode !== 200) return "error: "+ response.statusCode;
-    var csvLinesArray = data.trim().replace(/\"/g,"").split("\n");
-    var line8 = csvLinesArray[8].split(",");
-    updateTimestamp = {
-        "update-date": csvLinesArray[1].split(",")[2].split("-")[1],
-        "lost": line8[4]+","+line8[5],
-        "injured": line8[6]+","+line8[7]
-    };
-    var archiveFilename = "data-"+updateTimestamp["update-date"].replace(/:/g,".").replace(/ /g,"-")+"-NST.csv";
-    
-    fs.writeFile(archiveFilename, data, function(err){
-        if(err) throw err;
-        console.log("data archived in "+ archiveFilename);
-    });
-});
+var updateTimestamp = {};
 
 function updateFile(filename){
 
@@ -48,11 +31,35 @@ fs.readFile(filename, {
 });
 }
 
-updateFile("../index.html");
-updateFile("../iframe.html");
+request("https://docs.google.com/spreadsheets/d/1tGQlS3lIW077sOUIJ1qO0Rtaz34V-CxKEySFQ2W2CgA/export?format=csv&gid=1901101589", function(error, response, data){
+    if (error || response.statusCode !== 200) return "error: "+ response.statusCode;
+    var csvLinesArray = data.trim().replace(/\"/g,"").split("\n");
+    var line8 = csvLinesArray[8].split(",");
+    updateTimestamp = {
+        "update-date": csvLinesArray[1].split(",")[2].split("-")[1],
+        "lost": line8[4]+","+line8[5],
+        "injured": line8[6]+","+line8[7]
+    };
+
+
+    updateFile("../index.html");
+    updateFile("../iframe.html");
+
+    var archiveFilename = "data-"+updateTimestamp["update-date"].replace(/:/g,".").replace(/ /g,"-")+"-NST.csv";
+    
+    fs.writeFile(archiveFilename, data, function(err){
+        if(err) throw err;
+        console.log("data archived in "+ archiveFilename);
+    });
+});
 
 request
   .get("https://docs.google.com/spreadsheets/d/1tGQlS3lIW077sOUIJ1qO0Rtaz34V-CxKEySFQ2W2CgA/export?format=csv&gid=504630038")
   .on('error', function(err) {
     console.log(err)
   }).pipe(fs.createWriteStream("data.csv"));
+
+
+
+
+
