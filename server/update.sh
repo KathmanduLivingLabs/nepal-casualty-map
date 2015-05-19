04b2e2d764b23c0badbc255ea0f04b1d5bf0ddb7
jediKnight <gautamxpratik@gmail.com>
2015-05-19 13:37:51 +0545

    Update update.sh
    cd .. to the root before git add -A for compliance with git v2.0 (current version is git 1.9.1, therefore no issues yet)

--- server/update.sh
+++ server/update.sh
@@ -3,6 +3,7 @@ git pull origin gh-pages
 cd ../data
 node ../server/fetcher-archiver.js
 topojson -o districts_topo.json --id-property 'distID, distID' --external-properties=data.csv --properties='district=district' --properties='dead=dead' --properties='injured=injured' --properties='GovComplete=GovComplete' --properties='GovPartial=GovPartial' --properties='OthComplete=OthComplete' --properties='OthPartial=OthPartial' --properties='population=population' --properties='deadpercent=deadpercent' --properties='injuredpercent=injuredpercent' -- districts_id.json;
+cd ..
 git add -A
 git commit -a -m "Updating data"
 git push origin gh-pages
